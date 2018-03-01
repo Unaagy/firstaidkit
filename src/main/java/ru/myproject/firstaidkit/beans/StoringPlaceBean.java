043465f465a5879910cebbee9d0c1a0f37be86d0
Tuchnyak <georgii.o.sv@gmail.com>
2018-03-01 22:23:04 +0300

    StoringPlaceBean has been added

---
+++ src/main/java/ru/myproject/firstaidkit/beans/StoringPlaceBean.java
@@ -0,0 +1,40 @@
+package ru.myproject.firstaidkit.beans;
+
+import ru.myproject.firstaidkit.entity.StoringPlace;
+
+import java.util.List;
+
+public class StoringPlaceBean {
+
+    private List<StoringPlace> storingPlaces;
+    private StoringPlace storingPlace;
+    private long id;
+
+    public StoringPlaceBean() {
+
+    }
+
+    public List<StoringPlace> getStoringPlaces() {
+        return storingPlaces;
+    }
+
+    public void setStoringPlaces(List<StoringPlace> storingPlaces) {
+        this.storingPlaces = storingPlaces;
+    }
+
+    public StoringPlace getStoringPlace() {
+        return storingPlace;
+    }
+
+    public void setStoringPlace(StoringPlace storingPlace) {
+        this.storingPlace = storingPlace;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
